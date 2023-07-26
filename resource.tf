bf712e071450728ad2e8b07f8127879e1733135c
cumberland-cloud <the.cumberland.cloud@gmail.com>
2023-07-26 18:16:35 -0400

    enable key rotation

--- resource.tf
+++ resource.tf
@@ -1,6 +1,7 @@
 resource "aws_kms_key" "this" {
   description               = var.key.description
   deletion_window_in_days   = 10
+  enable_key_rotation       = true
   policy                    = local.policy
 }
 
