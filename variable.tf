d7ba6c7dd35e69b5319a0dda911e7b159d59f393
cumberland-cloud <the.cumberland.cloud@gmail.com>
2023-07-26 18:02:56 -0400

    typo

--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
-varible "key" {
+variable "key" {
     description     = "KMS Key configuration. Policy should be JSON string, and is applied in addition to the default KMS policy."
     type            = object({
         alias       = string
