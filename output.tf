49d0d7637582d74fde60c9031badfb4a771e0877
Grant Moore <chinchalinchin@gmail.com>
2023-07-27 16:19:23 -0400

    upodate docs

--- output.tf
+++ output.tf
@@ -1,6 +1,8 @@
 output "key" {
-    value       = {
-        id      = aws_kms_key.this.key_id
-        arn     = aws_kms_key.this.arn
+    description     = "Metadata of the KMS Key that has been provisioned"
+    value           = {
+        id          = aws_kms_key.this.key_id
+        arn         = aws_kms_key.this.arn
+        alias_arn   = aws_kms_alias.arn
     }
 }
