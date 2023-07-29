51a8ddd6825be1a2b10f8559adfb1b7b77a2aa28
Grant Moore <chinchalinchin@gmail.com>
2023-07-29 09:33:29 -0400

    data address

--- data.tf
+++ data.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "merged" {
     count                   = local.merge ? 1 : 0
 
     source_policy_documents = [
-        aws_iam_policy_document.unmerged.json,
+        data.aws_iam_policy_document.unmerged.json,
         var.key.policy
     ]
 }
