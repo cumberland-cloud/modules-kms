6e0a564ce2de12f66dcd8f0cc2090d7bf23e141c
Grant Moore <chinchalinchin@gmail.com>
2023-07-29 09:35:28 -0400

    typo

--- output.tf
+++ output.tf
@@ -3,6 +3,6 @@ output "key" {
     value           = {
         id          = aws_kms_key.this.key_id
         arn         = aws_kms_key.this.arn
-        alias_arn   = aws_kms_alias.arn
+        alias_arn   = aws_kms_alias.this.arn
     }
 }
